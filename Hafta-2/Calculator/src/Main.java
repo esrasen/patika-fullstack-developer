fd3f8af6cc3b34a34f4e3c8090237858ed4cc411
Esra Şen <sen.esra@yahoo.com>
2023-12-15 16:19:01 +0300

    Added rectangle area calculation feature to the calculator

---
+++ Hafta-2/Calculator/src/Main.java
@@ -0,0 +1,208 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void plus() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 0, i = 1;
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+            if (number == 0) {
+                break;
+            }
+            result += number;
+        }
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void minus() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        int number, result = 0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextInt();
+            if (i == 1) {
+                result += number;
+                continue;
+            }
+            result -= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void times() {
+        Scanner scan = new Scanner(System.in);
+        int number, result = 1, i = 1;
+
+        while (true) {
+            System.out.print(i++ + ". sayı :");
+            number = scan.nextInt();
+
+            if (number == 1)
+                break;
+
+            if (number == 0) {
+                result = 0;
+                break;
+            }
+            result *= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void divided() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Kaç adet sayı gireceksiniz :");
+        int counter = scan.nextInt();
+        double number, result = 0.0;
+
+        for (int i = 1; i <= counter; i++) {
+            System.out.print(i + ". sayı :");
+            number = scan.nextDouble();
+            if (i != 1 && number == 0) {
+                System.out.println("Böleni 0 giremezsiniz.");
+                continue;
+            }
+            if (i == 1) {
+                result = number;
+                continue;
+            }
+            result /= number;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+    static void power() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değeri giriniz :");
+        int base = scan.nextInt();
+        System.out.print("Üs değeri giriniz :");
+        int exponent = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+
+    static void factorial() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz :");
+        int n = scan.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        System.out.println("Sonuç : " + result);
+    }
+
+
+    static void mod() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bölünen değeri giriniz :");
+        int dividend = scan.nextInt();
+        System.out.print("Bölen değeri giriniz :");
+        int divisor = scan.nextInt();
+
+        int result = 1;
+
+        for (int i = 1; i <= dividend; i++) {
+            result = dividend - (divisor * i);
+            if (result < divisor) {
+                break;
+            }
+        }
+        System.out.println("Sonuç : " + result);
+
+    }
+
+    static void rectangle() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Dikdörtgen 1.kenar değerini giriniz :");
+        int longEdge = scan.nextInt();
+        if (longEdge <= 0) {
+            System.out.println("Kenar değeri 0'dan büyük olmalıdır.");
+            return;
+        }
+        System.out.print("Dikdörtgen 2.kenar değerini giriniz :");
+        int shortEdge = scan.nextInt();
+        if (shortEdge <= 0) {
+            System.out.println("Kenar değeri 0'dan büyük olmalıdır.");
+            return;
+        }
+
+        if (longEdge == shortEdge) {
+            System.out.println("Bu bir karedir.");
+        }
+        int area = longEdge * shortEdge;
+        int perimeter = 2 * (longEdge + shortEdge);
+
+        System.out.println("Dikdörtgenin alanı : " + area);
+        System.out.println("Dikdörtgenin çevresi : " + perimeter);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int select;
+        String menu = "1- Toplama İşlemi\n"
+                + "2- Çıkarma İşlemi\n"
+                + "3- Çarpma İşlemi\n"
+                + "4- Bölme işlemi\n"
+                + "5- Üslü Sayı Hesaplama\n"
+                + "6- Faktoriyel Hesaplama\n"
+                + "7- Mod Alma\n"
+                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
+                + "0- Çıkış Yap";
+
+        do {
+            System.out.println(menu);
+            System.out.print("Lütfen bir işlem seçiniz :");
+            select = scan.nextInt();
+            switch (select) {
+                case 1:
+                    plus();
+                    break;
+                case 2:
+                    minus();
+                    break;
+                case 3:
+                    times();
+                    break;
+                case 4:
+                    divided();
+                    break;
+                case 5:
+                    power();
+                    break;
+                case 6:
+                    factorial();
+                    break;
+                case 7:
+                    mod();
+                    break;
+                case 8:
+                    rectangle();
+                    break;
+                case 0:
+                    break;
+                
+                default:
+                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+            }
+        } while (select != 0);
+
+
+    }
+}
